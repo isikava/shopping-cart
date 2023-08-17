c84a0c03df1fbc07f12647f0fa4a70dc9c071ff6
Alexander Ignatev <alexign473@gmail.com>
2023-08-17 18:05:37 +0300

    Absolute imports

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,5 @@
-import { Link } from 'react-router-dom';
-import { Header } from '../components/Header';
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/components/Header';
 
 const Root = () => {
   return (
