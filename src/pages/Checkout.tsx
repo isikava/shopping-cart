9dfff3654dad8dfcb22cdb578a1a9971162b5b5b
Alexander Ignatev <alexign473@gmail.com>
2023-08-18 12:03:08 +0300

    Add theme, custom fonts and colors

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -83,7 +83,7 @@ const Checkout = () => {
               <Checkbox defaultChecked>Ship to billing address.</Checkbox>
             </GridItem>
             <GridItem colSpan={2}>
-              <Button size={'lg'} w={'full'}>
+              <Button colorScheme="brand" size={'lg'} w={'full'}>
                 Place Order
               </Button>
             </GridItem>
