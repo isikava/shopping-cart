8b919886d4c06cba23d17d6780a3bd1ed54521d8
Alexander Ignatev <alexign473@gmail.com>
2023-08-18 19:30:48 +0300

    Override Components styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,24 +1,93 @@
-import { extendTheme, theme as base } from '@chakra-ui/react';
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from '@chakra-ui/react';
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      50: '#f5fee5',
-      100: '#e1fbb2',
-      200: '#cdf781',
-      300: '#b8ee56',
-      400: '#a2e032',
-      500: '#8ac919',
-      600: '#71ab09',
-      700: '#578602',
-      800: '#3c5e00',
-      900: '#203300',
+const inputSelectStyles = {
+  variants: {
+    filled: {
+      field: {
+        _focus: {
+          borderColor: 'brand.400',
+        },
+      },
     },
   },
-  fonts: {
-    heading: `Montserrat, ${base.fonts?.heading}`,
-    body: `Inter, ${base.fonts?.body}`,
+  sizes: {
+    md: {
+      field: {
+        borderRadius: 'none',
+      },
+    },
   },
-});
+};
+
+const brandRing = {
+  _focus: {
+    ring: 2,
+    ringColor: 'brand.400',
+  },
+};
+
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        '50': '#FCE8E8',
+        '100': '#F7BFBF',
+        '200': '#F29797',
+        '300': '#ED6E6E',
+        '400': '#E84545',
+        '500': '#E31C1C',
+        '600': '#B61616',
+        '700': '#881111',
+        '800': '#5B0B0B',
+        '900': '#2D0606',
+      },
+    },
+    fonts: {
+      heading: `Montserrat, ${base.fonts?.heading}`,
+      body: `Inter, ${base.fonts?.body}`,
+    },
+    components: {
+      Input: { ...inputSelectStyles },
+      Select: { ...inputSelectStyles },
+      Checkbox: {
+        baseStyle: {
+          control: {
+            borderRadius: 'none',
+            ...brandRing,
+          },
+        },
+      },
+      Button: {
+        variants: {
+          primary: {
+            rounded: 'none',
+            ...brandRing,
+            color: 'white',
+            bg: 'brand.400',
+            _hover: {
+              bg: 'brand.500',
+            },
+            _active: {
+              bg: 'brand.600',
+            },
+          },
+        },
+      },
+    },
+  },
+  withDefaultColorScheme({
+    colorScheme: 'brand',
+    components: ['Checkbox'],
+  }),
+  withDefaultVariant({
+    variant: 'filled',
+    components: ['Input', 'Select'],
+  })
+);
 
 export default theme;
