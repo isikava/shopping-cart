065f5e305e3d0031cc9b274ad6701de557c1f3f7
Alexander Ignatev <alexign473@gmail.com>
2023-08-28 14:52:04 +0300

    Empty cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -14,6 +14,7 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   Stack,
+  Center,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { PRODUCTS, PRODUCTS_WOMEN } from '@/data';
@@ -21,7 +22,7 @@ import { Button } from '@/components/Button';
 import { MobileCart } from './elements/MobileCart';
 import { DesktopCart } from './elements/DesktopCart';
 import { deleteFromCart, updateQuantity } from '../Router';
-import { calcTotal } from './utils';
+import { calcQuantity, calcTotal } from './utils';
 
 export type CartProps = {
   cart: ICartItem[];
@@ -64,94 +65,108 @@ export const Cart = ({
       </VStack>
 
       {/* Cart View */}
-      <Stack
-        w={'full'}
-        direction={['column', null, null, 'row']}
-        spacing={[8, 16]}
-      >
-        {/* Cart Items */}
-        <Box display={['block', 'none']}>
-          <MobileCart
-            cart={cart}
-            onDeleteFromCart={onDeleteFromCart}
-            onUpdateQuantity={onUpdateQuantity}
-          />
-        </Box>
-        <Box display={['none', 'block']} maxW={'4xl'}>
-          <DesktopCart
-            cart={cart}
-            onDeleteFromCart={onDeleteFromCart}
-            onUpdateQuantity={onUpdateQuantity}
-          />
-        </Box>
+      {cart.length > 0 ? (
+        <Stack
+          w={'full'}
+          direction={['column', null, null, 'row']}
+          spacing={[8, 16]}
+        >
+          {/* Cart Items */}
+          <Box display={['block', 'none']}>
+            <MobileCart
+              cart={cart}
+              onDeleteFromCart={onDeleteFromCart}
+              onUpdateQuantity={onUpdateQuantity}
+            />
+          </Box>
+          <Box display={['none', 'block']} maxW={'4xl'}>
+            <DesktopCart
+              cart={cart}
+              onDeleteFromCart={onDeleteFromCart}
+              onUpdateQuantity={onUpdateQuantity}
+            />
+          </Box>
 
-        {/* Total */}
-        <VStack flex={1} align={'stretch'} spacing={[1, 4, 6]}>
-          <Heading
-            fontSize={['md', '2xl']}
-            fontWeight={'normal'}
-            color={['gray1', 'black']}
-            textTransform={['uppercase', 'initial']}
-          >
-            Apply Discount Code
-          </Heading>
-          <InputGroup>
-            <Input placeholder="Enter discount code" />
-            <InputRightElement>
-              <CButton variant={'muted'} px={10} mr={10}>
-                Apply
-              </CButton>
-            </InputRightElement>
-          </InputGroup>
+          {/* Total */}
+          <VStack flex={1} align={'stretch'} spacing={[1, 4, 6]}>
+            <Heading
+              fontSize={['md', '2xl']}
+              fontWeight={'normal'}
+              color={['gray1', 'black']}
+              textTransform={['uppercase', 'initial']}
+            >
+              Apply Discount Code
+            </Heading>
+            <InputGroup>
+              <Input placeholder="Enter discount code" />
+              <InputRightElement>
+                <CButton variant={'muted'} px={10} mr={10}>
+                  Apply
+                </CButton>
+              </InputRightElement>
+            </InputGroup>
 
-          <VStack
-            w={'full'}
-            alignItems={'stretch'}
-            p={4}
-            spacing={4}
-            bg={'lighthover'}
-            border={'2px'}
-            borderColor={'divider'}
-            fontFamily={'heading'}
-          >
-            <HStack justifyContent={'space-between'}>
-              <Heading
-                color={'gray1'}
-                fontSize={['sm', 'lg']}
-                fontWeight={'medium'}
-              >
-                Subtotal
-              </Heading>
-              <Text fontSize={['sm', 'lg']} fontWeight={'medium'}>
-                {total} EUR
-              </Text>
-            </HStack>
-            <HStack justifyContent={'space-between'}>
-              <Heading
-                color={'gray1'}
-                fontSize={['sm', 'lg']}
-                fontWeight={'medium'}
-              >
-                Tax
-              </Heading>
-              <Text fontSize={['sm', 'lg']} fontWeight={'medium'}>
-                0.00 EUR
-              </Text>
-            </HStack>
-            <HStack justifyContent={'space-between'}>
-              <Heading fontSize={['lg', '2xl']} fontWeight={'medium'}>
-                Order Total
-              </Heading>
-              <Text fontSize={['lg', '2xl']} fontWeight={'medium'}>
-                {total} EUR
-              </Text>
-            </HStack>
-            <Button variant={'brand'} size={['md', 'lg']}>
-              Proceed to checkout
-            </Button>
+            <VStack
+              w={'full'}
+              alignItems={'stretch'}
+              p={4}
+              spacing={4}
+              bg={'lighthover'}
+              border={'2px'}
+              borderColor={'divider'}
+              fontFamily={'heading'}
+            >
+              <HStack justifyContent={'space-between'}>
+                <Heading
+                  color={'gray1'}
+                  fontSize={['sm', 'lg']}
+                  fontWeight={'medium'}
+                >
+                  Subtotal
+                </Heading>
+                <Text fontSize={['sm', 'lg']} fontWeight={'medium'}>
+                  {total} EUR
+                </Text>
+              </HStack>
+              <HStack justifyContent={'space-between'}>
+                <Heading
+                  color={'gray1'}
+                  fontSize={['sm', 'lg']}
+                  fontWeight={'medium'}
+                >
+                  Tax
+                </Heading>
+                <Text fontSize={['sm', 'lg']} fontWeight={'medium'}>
+                  0.00 EUR
+                </Text>
+              </HStack>
+              <HStack justifyContent={'space-between'}>
+                <Heading fontSize={['lg', '2xl']} fontWeight={'medium'}>
+                  Order Total
+                </Heading>
+                <Text fontSize={['lg', '2xl']} fontWeight={'medium'}>
+                  {total} EUR
+                </Text>
+              </HStack>
+              <Button variant={'brand'} size={['md', 'lg']}>
+                Proceed to checkout
+              </Button>
+            </VStack>
           </VStack>
-        </VStack>
-      </Stack>
+        </Stack>
+      ) : (
+        <Center
+          h={[40, 'md']}
+          flexDirection={'column'}
+          gap={10}
+          textAlign={'center'}
+        >
+          <Heading>Your cart is empty</Heading>
+          <CButton as={Link} to="/products" variant={'brand'} size={'lg'}>
+            Shop now
+          </CButton>
+        </Center>
+      )}
     </Container>
   );
 };
